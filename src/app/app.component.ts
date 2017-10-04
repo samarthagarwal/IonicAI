6d5126a821f9c84956b99c09ed842b10103bb87c
unknown <samarthagarwal@live.com>
2017-10-05 02:11:35 +0530

    Stage2TextInteraction Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,14 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+
+declare var window;
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -16,6 +19,20 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      window["ApiAIPlugin"].init(
+        {
+          clientAccessToken: "015b8095146b4cfeac87a9b25e2181b8", // insert your client access key here 
+          lang: "en" // set lang tag from list of supported languages 
+        },
+        function (result) { 
+          alert(result)
+         },
+        function (error) { 
+          alert(error)
+         }
+      );
+
     });
   }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+declare var window;
 
 @Component({
   selector: 'page-home',
@@ -8,18 +10,44 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   messages: any[] = [];
+  text: string = "";
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public ngZone: NgZone) {
 
     this.messages.push({
       text: "Hi, how can I help you?",
       sender: "api"
     })
 
+  }
+
+  sendText(){
+
+    let message = this.text;
+
     this.messages.push({
-      text: "Hello",
-      sender: "me"
+      text: message,
+      sender: 'me'
+    });
+
+    this.text = "";
+
+    window["ApiAIPlugin"].requestText({
+      query: message
+    }, (response)=>{
+
+      this.ngZone.run(()=>{
+        this.messages.push({
+          text: response.result.fulfillment.speech,
+          sender: "api"
+        });
+      })
+
+      
+    }, (error)=> {
+      alert(JSON.stringify(error))
     })
+
 
   }
 
